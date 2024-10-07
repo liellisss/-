df1b3bd20c5b156de75e958bf292080ef548b334
Elizaveta <lizalisovenkoYT@gmail.com>
2024-10-07 14:37:25 +0300

    Add files via upload

---
+++ Clavisha.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using калькуляторчик;
+
+namespace калькуляторчик.Classes
+{
+    public class Clavisha
+    {
+        static public void OperationClick(string _operation)
+        {
+            Form1.Memory.Text = "";
+
+            switch (_operation)
+            {
+                case "+":
+                    Form1.TextBox.Text = (Form1.Value + Double.Parse(Form1.TextBox.Text)).ToString();
+                    break;
+                case "-":
+                    Form1.TextBox.Text = (Form1.Value - Double.Parse(Form1.TextBox.Text)).ToString();
+                    break;
+                case "*":
+                    Form1.TextBox.Text = (Form1.Value * Double.Parse(Form1.TextBox.Text)).ToString();
+                    break;
+                case "/":
+                    if (Double.Parse(Form1.TextBox.Text) == 0)
+                    {
+                        MessageBox.Show("На ноль делить плохо!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        Form1.TextBox.Text = (Form1.Value / Double.Parse(Form1.TextBox.Text)).ToString();
+                    }
+                    break;
+                case "^":
+                    Form1.TextBox.Text = (Math.Pow(Form1.Value, Double.Parse(Form1.TextBox.Text))).ToString();
+                    break;
+                case "log":
+                    Form1.TextBox.Text = (Math.Log(Form1.Value, Double.Parse(Form1.TextBox.Text))).ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    public class Clavisha2
+    {
+        static public void OperationClick(string _operation)
+        {
+
+            switch (_operation)
+            {
+                case "n!":
+                    double k = 1;
+                    for (int i = 1; i <= Double.Parse(Form1.TextBox.Text); i++)
+                        k = k * i;
+                    Form1.TextBox.Text = k.ToString();
+                    break;
+                case "√":
+                    Form1.TextBox.Text = (Math.Sqrt(Double.Parse(Form1.TextBox.Text))).ToString();
+                    break;
+                case "^2":
+                    //Console.WriteLine(Form1.Value);
+                    Form1.TextBox.Text = (Math.Pow(Double.Parse(Form1.TextBox.Text), 2)).ToString();
+                    break;
+                case "π":
+                    Form1.TextBox.Text = (Math.PI).ToString();
+                    break;
+                case "1/x":
+                    Form1.TextBox.Text = (1 / Double.Parse(Form1.TextBox.Text)).ToString();
+                    break;
+                case "sin":
+                    double angleInDegreesSin = Double.Parse(Form1.TextBox.Text);
+                    double angleInRadiansSin = angleInDegreesSin * Math.PI / 180;
+                    Form1.TextBox.Text = Math.Sin(angleInRadiansSin).ToString();
+                    break;
+                case "cos":
+                    double angleInDegreesCos = Double.Parse(Form1.TextBox.Text);
+                    double angleInRadiansCos = angleInDegreesCos * Math.PI / 180;
+                    Form1.TextBox.Text = Math.Cos(angleInRadiansCos).ToString();
+                    break;
+                case "|x|":
+                    Form1.TextBox.Text = Math.Abs(Double.Parse(Form1.TextBox.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using калькуляторчик.Classes;
+
+namespace калькуляторчик
+{
+    public partial class Form1 : Form
+    {
+        static TextBox _textBox;
+        public static TextBox TextBox { get=> _textBox; set => _textBox = value; }
+        static Label _memory;
+        public static Label Memory { get=> _memory; set => _memory = value; }
+
+        static Double _value = 0;
+        public static Double Value { get=>_value; set => _value = value; }
+
+        String operation = "";
+        Boolean operation_pressed = false;
+        public Form1() { 
+            InitializeComponent();
+            InitVariables();
+        }
+
+        private void operation_click(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            operation = bt.Text;
+            _value = Double.Parse(result.Text);
+            operation_pressed = true;
+            memory.Text = $"{_value} {operation}";
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button bt=(Button)sender;
+            operation = bt.Text;
+            Clavisha2.OperationClick(operation);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((_textBox.Text == "0") || (operation_pressed))
+                _textBox.Clear();
+            
+            operation_pressed = false;
+            Button bt=(Button)sender; 
+
+            if(_textBox.Text.StartsWith("0") && bt.Text != ",")
+            {
+                if(_textBox.Text.Length > 1 && _textBox.Text[1] != ',')
+                {
+                    _textBox.Text = _textBox.Text.Remove(0, 1);
+                }
+            }
+
+            _textBox.Text += bt.Text;
+
+            if(_textBox.Text == ",")
+            {
+                _textBox.Text = "0,";
+            }
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            _textBox.Text = "0";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Clavisha.OperationClick(operation);
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            _textBox.Clear();
+            _value = 0;
+        }
+
+        void InitVariables()
+        {
+            _textBox = result;
+            _memory = memory;
+        }
+
+    }
+}
